33a72cb542a3e4ca31f7f948dcad833382b57d44
CyberWordPL <cyberwordixx@gazeta.pl>
2025-03-30 10:35:49 +0200

    little refactor and addition of da_foreach

--- examples/dyn_array_1.c
+++ examples/dyn_array_1.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <stddef.h>
 
 #define HOU_IMPLEMENTATION
 #include "../hou.h"
 
 generate_da_type(int);
+
+void foreach_print_elements(size_t index, int *element) {
+  printf("element %d is %d\n", index, *element);
+}
+
+void foreach_add_one(size_t index, int *element) {
+  (*element)++;
+}
 
 void main() {
   da_int da = create_da(int);
@@ -15,10 +24,9 @@ void main() {
   remove_last_from_da((&da)); // 1 2 5
   append_to_da((&da), 7); // 1 2 5 7
   insert_da((&da), 3, 3); // 1 2 5 3 7
-  remove_from_index_da((&da), 3) // 1 2 3 7
+  remove_from_index_da((&da), 3); // 1 2 3 7
   
-
-  for (int i = 0; i < da.arrUsed; i++) {
-    printf("%dth element is: %d\n", i, da.arr[i]);
-  }
+  da_foreach(da, foreach_print_elements);
+  da_foreach(da, foreach_add_one);
+  da_foreach(da, foreach_print_elements);
 }
--- hou.h
+++ hou.h
@@ -29,7 +29,7 @@
 #endif
 
 HOU_TMP_SPEC bool __hou_stored_ptr_lock = false;
-HOU_TMP_SPEC void *__hou_stored_ptr = NULL;
+HOU_TMP_SPEC void* __hou_stored_ptr = NULL;
 
 /**
    Fail
@@ -49,31 +49,30 @@ HOU_TMP_SPEC void *__hou_stored_ptr = NULL;
 #endif
 
 typedef struct {
-  void *arr;
+  void* arr;
   size_t arrAllocated;
   size_t arrUsed;
 } da_general; 
 
-#define generate_da_type(type) typedef struct {			\
-    type *arr;							\
-    size_t arrAllocated;					\
-    size_t arrUsed;						\
-  } da_##type;							\
-								\
-  da_##type __create_da_##type() {				\
-    da_##type *p = create_da_ptr(type);				\
-    da_##type v = *p;						\
-    free(p);							\
-    return v;							\
-  }
-  
+#define generate_da_type(type) typedef struct {				\
+    type* arr;								\
+    size_t arrAllocated;						\
+    size_t arrUsed;							\
+  } da_##type;								\
+									\
+  da_##type __create_da_##type() {					\
+    da_##type* p = create_da_ptr(type);					\
+    da_##type v = *p;							\
+    free(p);								\
+    return v;								\
+  }									
 
 HOU_FUNC_SPEC da_general create_da_general(size_t elem_size);
 HOU_FUNC_SPEC da_general *create_allocated_da_general(size_t elem_size);
-HOU_FUNC_SPEC void append_to_da_general(da_general *da, size_t element_sizeof, const void *element);
-HOU_FUNC_SPEC void remove_last_from_da_general(da_general *da, size_t element_sizeof);
-HOU_FUNC_SPEC void insert_da_general(da_general *da, size_t element_sizeof, const void *element, size_t index);
-HOU_FUNC_SPEC void remove_from_index_da_general(da_general *da, size_t element_sizeof, size_t index);
+HOU_FUNC_SPEC void append_to_da_general(da_general* da, size_t element_sizeof, const void* element);
+HOU_FUNC_SPEC void remove_last_from_da_general(da_general* da, size_t element_sizeof);
+HOU_FUNC_SPEC void insert_da_general(da_general* da, size_t element_sizeof, const void* element, size_t index);
+HOU_FUNC_SPEC void remove_from_index_da_general(da_general* da, size_t element_sizeof, size_t index);
 
 #define create_da_ptr(type) (da_##type*)create_allocated_da_general(sizeof(type)); 
 
@@ -86,7 +85,7 @@ HOU_FUNC_SPEC void remove_from_index_da_general(da_general *da, size_t element_s
   __hou_stored_ptr = malloc(sizeof(*(da->arr)));			\
   fail_if(__hou_stored_ptr != NULL, "Allocation Error");		\
   *(typeof(element)*)__hou_stored_ptr = element;			\
-  append_to_da_general((da_general *)da, sizeof(*(da->arr)), __hou_stored_ptr); \
+  append_to_da_general((da_general* )da, sizeof(*(da->arr)), __hou_stored_ptr); \
   free(__hou_stored_ptr);						\
   __hou_stored_ptr_lock = false;
 
@@ -97,7 +96,7 @@ HOU_FUNC_SPEC void remove_from_index_da_general(da_general *da, size_t element_s
 // FOR PEOPLE THAT THINK THAT THING THIS CAN BREAK:
 // IT SHOULDN'T: THERE IS THE SAME STRUCTURE OF STRUCT
 #define remove_last_from_da(da) \
-  remove_last_from_da_general((da_general *)da, sizeof(*da->arr));
+  remove_last_from_da_general((da_general* )da, sizeof(*da->arr));
 
 #define insert_da(da, element, index)					\
   fail_if(sizeof(element) == sizeof(*(da->arr)), "Your element isn't type of element in array"); \
@@ -106,12 +105,15 @@ HOU_FUNC_SPEC void remove_from_index_da_general(da_general *da, size_t element_s
   __hou_stored_ptr = malloc(sizeof(*(da->arr)));			\
   fail_if(__hou_stored_ptr != NULL, "Allocation Error");		\
   *(typeof(element)*)__hou_stored_ptr = element;			\
-  insert_da_general((da_general *)da, sizeof(*(da->arr)), __hou_stored_ptr, index); \
+  insert_da_general((da_general* )da, sizeof(*(da->arr)), __hou_stored_ptr, index); \
   free(__hou_stored_ptr);						\
   __hou_stored_ptr_lock = false;
 
 #define remove_from_index_da(da, index)					\
-  remove_from_index_da_general((da_general *)da, sizeof(*da->arr), index);
+  remove_from_index_da_general((da_general* )da, sizeof(*da->arr), index);
+
+#define da_foreach(da, function_name) \
+  for(int i = 0; i < da.arrUsed; i++) function_name(i, &(da.arr[i]))
 
 #ifdef HOU_IMPLEMENTATION
 
@@ -124,7 +126,7 @@ static da_general create_da_general(size_t elem_size) {
   return da;
 }
 
-static da_general *create_allocated_da_general(size_t elem_size) {
+static da_general* create_allocated_da_general(size_t elem_size) {
   da_general *da = malloc(sizeof(da_general));
   fail_if(da != NULL, "Allocation Error");
 
@@ -132,10 +134,10 @@ static da_general *create_allocated_da_general(size_t elem_size) {
   return da;
 }
 
-static void append_to_da_general(da_general *da, size_t element_sizeof, const void *element) {
+static void append_to_da_general(da_general* da, size_t element_sizeof, const void* element) {
   da->arrUsed++;
   if(da->arrUsed > da->arrAllocated) {
-    void * realloced = realloc(da->arr, (da->arrAllocated + HOU_DA_CHUNK_SIZE) * element_sizeof);
+    void* realloced = realloc(da->arr, (da->arrAllocated + HOU_DA_CHUNK_SIZE) * element_sizeof);
     if(realloced == NULL) {
       free(da->arr);
       fail_if(true, "Allocaton Error");
@@ -144,14 +146,14 @@ static void append_to_da_general(da_general *da, size_t element_sizeof, const vo
   }
 
   // memcpy(da->arr + ((da->arrUsed - 1) * HOU_DA_CHUNK_SIZE), element, element_sizeof);
-  void *startingPoint = da->arr + ((da->arrUsed - 1) * element_sizeof);
+  void* startingPoint = da->arr + ((da->arrUsed - 1) * element_sizeof);
   memcpy(startingPoint, element, element_sizeof);
 }
 
-static void remove_last_from_da_general(da_general *da, size_t element_sizeof) {
+static void remove_last_from_da_general(da_general* da, size_t element_sizeof) {
   da->arrUsed--;
   if(da->arrUsed < da->arrAllocated - HOU_DA_CHUNK_SIZE) {
-    void * realloced = realloc(da->arr, (da->arrAllocated - HOU_DA_CHUNK_SIZE) * element_sizeof);
+    void* realloced = realloc(da->arr, (da->arrAllocated - HOU_DA_CHUNK_SIZE) * element_sizeof);
     if(realloced == NULL) {
       free(da->arr);
       fail_if(true, "Allocaton Error");
@@ -160,7 +162,7 @@ static void remove_last_from_da_general(da_general *da, size_t element_sizeof) {
   }
 }
 
-static void insert_da_general(da_general *da, size_t element_sizeof, const void *element, size_t index) {
+static void insert_da_general(da_general* da, size_t element_sizeof, const void* element, size_t index) {
   da->arrUsed++;
   if(da->arrUsed > da->arrAllocated) {
     void * realloced = realloc(da->arr, (da->arrAllocated + HOU_DA_CHUNK_SIZE) * element_sizeof);
@@ -171,7 +173,7 @@ static void insert_da_general(da_general *da, size_t element_sizeof, const void 
     da->arr == realloced;
   }
 
-  void *index_in_arr = da->arr + index * element_sizeof;
+  void* index_in_arr = da->arr + index * element_sizeof;
   
   // Moving data forward
   memmove(index_in_arr + element_sizeof, index_in_arr, (da->arrUsed - index) * element_sizeof);
@@ -180,15 +182,15 @@ static void insert_da_general(da_general *da, size_t element_sizeof, const void 
   memcpy(index_in_arr, element, element_sizeof);
 }
 
-static void remove_from_index_da_general(da_general *da, size_t element_sizeof, size_t index) {
+static void remove_from_index_da_general(da_general* da, size_t element_sizeof, size_t index) {
   da->arrUsed--;
 
-  void *index_in_arr = da->arr + index * element_sizeof;
+  void* index_in_arr = da->arr + index * element_sizeof;
   
   memmove(index_in_arr - element_sizeof, index_in_arr, (da->arrUsed - index + 1) * element_sizeof);
   
   if(da->arrUsed < da->arrAllocated - HOU_DA_CHUNK_SIZE) {
-    void * realloced = realloc(da->arr, (da->arrAllocated - HOU_DA_CHUNK_SIZE) * element_sizeof);
+    void* realloced = realloc(da->arr, (da->arrAllocated - HOU_DA_CHUNK_SIZE) * element_sizeof);
     if(realloced == NULL) {
       free(da->arr);
       fail_if(true, "Allocaton Error");
